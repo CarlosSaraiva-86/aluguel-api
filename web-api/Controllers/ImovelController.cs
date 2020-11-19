fa93137380b583103f640f5c8c75462013dd7d10
Carlos Saraiva <carlos@evolucaoinfo.com.br>
2020-11-19 15:49:31 -0300

    Controllers Imovel e proprietario

--- web-api/Controllers/ImovelController.cs
+++ web-api/Controllers/ImovelController.cs
@@ -8,5 +8,60 @@ namespace web_api.Controllers
     [ApiController]
     public class ImovelController : ControllerBase
     {
+        public readonly IImovelRepository repo;
+
+        public ImovelController(IImovelRepository _repo)
+        {
+            repo = _repo;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            return Ok(repo.GetAll());
+        }
+
+        [HttpGet("Get")]
+        public IActionResult Get(int id)
+        {
+            return Ok(repo.Get(id));
+        }
+
+        [HttpPost("Set")]
+        public IActionResult Set(Imovel model)
+        {
+            if (model != null)
+            {
+                repo.Set(model);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost("Delete")]
+        public IActionResult Delete(Imovel model)
+        {
+            if (model != null)
+            {
+                repo.Delete(model);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost("Update")]
+        public IActionResult Update(Imovel model)
+        {
+            if (model != null)
+            {
+                repo.Update(model);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
     }
 }
--- web-api/Controllers/ProprietarioController.cs
+++ web-api/Controllers/ProprietarioController.cs
@@ -8,5 +8,54 @@ namespace web_api.Controllers
     [ApiController]
     public class ProprietarioController : ControllerBase
     {
+        public readonly IProprietarioRepository repo;
+
+        public ProprietarioController(IProprietarioRepository _repo)
+        {
+            repo = _repo;
+        }
+
+        [HttpGet("Get")]
+        public IActionResult Get(int id)
+        {
+            return Ok(repo.Get(id));
+        }
+
+        [HttpPost("Set")]
+        public IActionResult Set(Proprietario model)
+        {
+            if (model != null)
+            {
+                repo.Set(model);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost("Delete")]
+        public IActionResult Delete(Proprietario model)
+        {
+            if (model != null)
+            {
+                repo.Delete(model);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost("Update")]
+        public IActionResult Update(Proprietario model)
+        {
+            if (model != null)
+            {
+                repo.Update(model);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
     }
 }
